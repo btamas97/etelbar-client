8275a866fa1bfea5051bc22607f3af1d875f7b11
btamas97 <tamas.barath@gmail.com>
2019-11-12 22:15:58 +0100

    Added unit test.

--- app/src/test/java/com/example/dreamer/etelbarclient/ExampleUnitTest.java
+++ app/src/test/java/com/example/dreamer/etelbarclient/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.dreamer.etelbarclient;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private Table table;
+
+    @Before
+    void init(){
+        table = new Table(1,1,true);
+    }
+
+    @Test
+    public void setFalse_isCorrect(){
+        table.setStatus(false);
+        assertEquals(false,table.isFree());
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
